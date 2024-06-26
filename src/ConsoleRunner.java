50cb4ec171f5f16115a7ae97f1668ad1865e9570
Bogatov Oleg <oleg.ferari2015@yandex.ru>
2024-06-26 16:06:41 +0300

    add decode and encode RUS

--- src/ConsoleRunner.java
+++ src/ConsoleRunner.java
@@ -8,6 +8,8 @@ public class ConsoleRunner extends CryptoAnalizer {
 
         console();
         encode("input.txt", "encrypted.txt", 3); // Кодируем input.txt с ключом 3
-        decode("encrypted.txt", "decrypted.txt", 3); // Декодированный encrypted.txt c ключом 3
+        decode("encrypted.txt", "decrypted.txt", 3);// Декодированный encrypted.txt c ключом 3
+        encodeRus("input.txt", "encryptedRus.txt", 3); // Аналогично для русского языка
+        decodeRus("encryptedRus.txt", "decryptedRus.txt", 3);
     }
 }
--- src/CryptoAnalizer.java
+++ src/CryptoAnalizer.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -31,5 +32,31 @@ public class CryptoAnalizer extends StartMessage {
 
     public static void decode(String inputFile, String outputFile, int key) {
         encode(inputFile, outputFile, 26 - key); // потому что в англ. алфавите 26 букв, а в русском 33
+    }
+
+    public static void encodeRus(String inputFile, String outputFile, int key) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                StringBuilder encryptedLine = new StringBuilder();
+                for (char c : line.toCharArray()) {
+                    if (Character.isLetter(c)) {
+                        char base = Character.isLowerCase(c) ? 'а' : 'А';
+                        encryptedLine.append((char) ((((c - base) + key) % 32) + base));
+                    } else {
+                        encryptedLine.append(c);
+                    }
+                }
+                writer.write(encryptedLine.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void decodeRus(String inputFile, String outputFile, int key) {
+        encodeRus(inputFile, outputFile, 32 - key); // Аналогично для русского языка
     }
 }
